69e4b01c242fc501e12fb59b10d7d95b89c41b67
amritansh-mishra <mamritansh1911@gmail.com>
2025-05-22 18:00:17 +0530

    3rd

---
+++ PrintingDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintingDigits {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number you want to print digits of:");
+        int number = scanner.nextInt();
+
+        while (number != 0) {
+            int digit = number % 10;  // get last digit
+            number /= 10;             // remove last digit
+            System.out.println(digit);
+        }
+
+        scanner.close();
+    }
+}
